4770e13f387cc429e37d4d4f4522d12f7517a34b
Zahid Hasan <zahidhasan140338@gmail.com>
2024-11-27 12:32:04 +0600

    completed the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import Navbar from "./sections/Navbar"
 
 
 const App = () => {
   return (
-    <main className="max-w-7xl mx-auto relative">
-      <h1 className="text-2xl text-white underline">
-          hello
-      </h1>
+    <div className="text-white">
+      <Navbar></Navbar>
+    </div>
+     
 
-    </main>
+  
   )
 }
 
